0cf32c4d5434e837d99c439f1c0e44e8e3370203
Snowy XD <mantas00kirsis@gmail.com>
2024-10-25 20:42:16 +0300

    Update app.py

--- poker_website/app.py
+++ poker_website/app.py
@@ -43,4 +43,4 @@ def log_to_excel(username, buy_in, cash_out, result):
 
 # Ensures that the Flask app runs only when this script is executed directly
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
